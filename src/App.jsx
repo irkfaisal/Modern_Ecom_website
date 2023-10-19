b761b9d4453d910a5180775facd36a776361c298
Md Faisal Raza Khan <irkfaisal@gmail.com>
2023-10-19 11:16:36 +0530

    navbar & hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import Nav from './Components/Nav'
+import { Hero } from './Sections'
 
 const App = () => {
   return (
     <main className='relative'>
       <Nav />
+      <section className='xl:padding-l wide:padding-r padding-b'>
+        <Hero />
+      </section>
     </main>
+
   )
 }
 
